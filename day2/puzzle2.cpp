def3817cc1fe8f8abe41d9e763d99beab5874da8
jubuyer <jahmed10@stuy.edu>
2021-12-02 00:31:33 -0500

    part 2

--- day2/puzzle2.cpp
+++ day2/puzzle2.cpp
@@ -29,8 +29,38 @@ void part1() {
    std::cout << ans << '\n';
 }
 
+void part2() {
+  int horizontal = 0;
+  int depth = 0;
+  int aim = 0;
+
+  std::string line;
+  std::fstream file;
+  file.open("course.csv");
+  if(file.is_open()) {
+     while(std::getline(file, line)) {
+       if(line.substr(0,2) == "fo") {
+         int i = std::stoi(line.substr(8,1));
+         horizontal += i;
+         depth += aim * i;
+       } else if(line.substr(0,2) == "do") {
+         int j = std::stoi(line.substr(5,1));
+         aim += j;
+       } else if(line.substr(0,2) == "up") {
+         int k = std::stoi(line.substr(3,1));
+         aim -= k;
+       }
+     }
+   }
+
+   int ans = horizontal * depth;
+
+   std::cout << ans << '\n';
+}
+
 int main(int argc, char const *argv[]) {
   part1();
-
+  std::cout << '\n';
+  part2();
   return 0;
 }
